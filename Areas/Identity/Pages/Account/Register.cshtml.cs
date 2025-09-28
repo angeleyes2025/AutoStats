1c0e221664e65369aad43074776ad2f496553d6d
angeleyes2025 <a_tahirovic@live.com>
2025-09-28 22:38:28 +0200

    Add files via upload

---
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,154 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using AutoStats.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace AutoStats.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<AutoStatsUser> _signInManager;
+        private readonly UserManager<AutoStatsUser> _userManager;
+        private readonly IUserStore<AutoStatsUser> _userStore;
+        private readonly IUserEmailStore<AutoStatsUser> _emailStore;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public RegisterModel(
+            UserManager<AutoStatsUser> userManager,
+            IUserStore<AutoStatsUser> userStore,
+            SignInManager<AutoStatsUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Ime je obavezno")]
+            [StringLength(50)]
+            [Display(Name = "Ime")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Prezime je obavezno")]
+            [StringLength(50)]
+            [Display(Name = "Prezime")]
+            public string LastName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "{0} mora imati najmanje {2}, a najviše {1} karaktera.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Lozinka")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Potvrdi lozinku")]
+            [Compare("Password", ErrorMessage = "Lozinke se ne poklapaju.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (ModelState.IsValid)
+            {
+                var user = CreateUser();
+
+                user.FirstName = Input.FirstName ?? "";
+                user.LastName = Input.LastName ?? "";
+
+
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Novi korisnik je registrovan.");
+
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Potvrda email adrese",
+                        $"Molimo potvrdite registraciju klikom <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>ovdje</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+
+        private AutoStatsUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<AutoStatsUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Ne mogu instancirati '{nameof(AutoStatsUser)}'. Provjeri da klasa ima parametarless konstruktor.");
+            }
+        }
+
+        private IUserEmailStore<AutoStatsUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("Email podrška nije omogućena.");
+            }
+            return (IUserEmailStore<AutoStatsUser>)_userStore;
+        }
+    }
+}
