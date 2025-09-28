e4d4a579abe9929cf830557da01680cb9c52be5f
angeleyes2025 <a_tahirovic@live.com>
2025-09-28 22:38:55 +0200

    Add files via upload

---
+++ Data/AutoStatsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AutoStats.Models;
+
+namespace AutoStats.Data
+{
+    public class AutoStatsContext : IdentityDbContext<AutoStatsUser>
+    {
+        public AutoStatsContext(DbContextOptions<AutoStatsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<ServiceRecord> ServiceRecords { get; set; }
+
+        // Dodaj druge DbSet<> entitete npr ako ih imamo (npr. Servise, Gume, itd.)
+    }
+}
---
+++ Data/AutoStatsUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoStats.Data
+{
+    public class AutoStatsUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string LastName { get; set; }
+
+        // Po potrebi dodaj: datum rođenja, adresa, itd.
+    }
+}
